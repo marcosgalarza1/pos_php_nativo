daf58b789b6688634bf06d1f999ead771b97ba32
Enrique Condori <econdori@digicert.bo>
2025-02-24 15:56:35 -0400

    add-menu-arqueo-de-caja

--- vistas/modulos/menu.php
+++ vistas/modulos/menu.php
@@ -75,15 +75,19 @@
 
 			if ($_SESSION["perfil"] == "Administrador" || $_SESSION["perfil"] == "") {
 
-				echo '<li>
-
-				<a href="meseros">
-				<img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABkAAAAZCAYAAADE6YVjAAAACXBIWXMAAAsTAAALEwEAmpwYAAACM0lEQVR4nO2US4jUQBCGI+LjJKigoKCCKF58sOIDPAjeRPAoghfBg4IXQdCTVy8exJsKngSFLILuznZXdtadhZlUZaCrsiyjggriA1dBRBBR8BHpzWQ2m0nigDmJBX1Id1Nf1d9/xXH++dBojmnkm4DmnNvpLK0cACTngSTqLZR7lUM0yZMFEJKo3m6vrhQCyC+ykFEM1xfdV0GwQlN4EkgmFMqpWnNm5SCd3EoDNMrjwrtohjTJy4y8bz0MD5RCJmhmLRBLDOB3ypc9efc8f3oNoMxmu44Xf6w3zYZSUBRFi1TLbDbGLMkFtMJd1hD5gEQBVoDhXqeowvQ3YGcVBLLTdd3Fyd54i9dplLtAXC9aGnlYU7ipl6jm80ZAua2J39sqvIB3J2f2MWOt+bVHZl+2KBWY/Rr5AhBftEsjn8mtHojvZDR15zvhaz0JiD97gTmUnGnk04D8Mz6T7xp50u7lQvocQvJjnGRLt4CpBVqTfNXER7VvzmrkX929q7mJ02+Q/3BywxrAOqXvLK46BqC8snKXQ0gOF9jwm0I5mEr8zM5MZiZmx9rTW0sBXakulVjxeSph005zIp9G+TCGZvsfAXOaozwo83uqsyl7X6mnywDDE3aOBgLMdYI82bXofUW8A4gvA/GbnDd6OHDSvk58s80m0EF4ZB5shgDlS2aCPedvI/0LAZLrfXKhkFNlaJKRHLk+WUtXB0G+AsiPFPFxIGFArtnJbzQayyuD/A8nJ34D3q7BBZ6gxLoAAAAASUVORK5CYII=">
-				<span>Meseros</span>
-				</a>
-				
-
-			</li>';
+			echo '
+				<li>
+					<a href="meseros">
+					<img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABkAAAAZCAYAAADE6YVjAAAACXBIWXMAAAsTAAALEwEAmpwYAAACM0lEQVR4nO2US4jUQBCGI+LjJKigoKCCKF58sOIDPAjeRPAoghfBg4IXQdCTVy8exJsKngSFLILuznZXdtadhZlUZaCrsiyjggriA1dBRBBR8BHpzWQ2m0nigDmJBX1Id1Nf1d9/xXH++dBojmnkm4DmnNvpLK0cACTngSTqLZR7lUM0yZMFEJKo3m6vrhQCyC+ykFEM1xfdV0GwQlN4EkgmFMqpWnNm5SCd3EoDNMrjwrtohjTJy4y8bz0MD5RCJmhmLRBLDOB3ypc9efc8f3oNoMxmu44Xf6w3zYZSUBRFi1TLbDbGLMkFtMJd1hD5gEQBVoDhXqeowvQ3YGcVBLLTdd3Fyd54i9dplLtAXC9aGnlYU7ipl6jm80ZAua2J39sqvIB3J2f2MWOt+bVHZl+2KBWY/Rr5AhBftEsjn8mtHojvZDR15zvhaz0JiD97gTmUnGnk04D8Mz6T7xp50u7lQvocQvJjnGRLt4CpBVqTfNXER7VvzmrkX929q7mJ02+Q/3BywxrAOqXvLK46BqC8snKXQ0gOF9jwm0I5mEr8zM5MZiZmx9rTW0sBXakulVjxeSph005zIp9G+TCGZvsfAXOaozwo83uqsyl7X6mnywDDE3aOBgLMdYI82bXofUW8A4gvA/GbnDd6OHDSvk58s80m0EF4ZB5shgDlS2aCPedvI/0LAZLrfXKhkFNlaJKRHLk+WUtXB0G+AsiPFPFxIGFArtnJbzQayyuD/A8nJ34D3q7BBZ6gxLoAAAAASUVORK5CYII=">
+					<span>Meseros</span>
+					</a>
+				</li>
+				<li>
+					<a href="arqueo-de-caja">
+						<i class="fa fa-inbox" aria-hidden="true"></i>
+						<span>Apertura de Caja</span>
+					</a>
+				</li>';
 			}
 
 			if ($_SESSION["perfil"] == "Administrador" || $_SESSION["perfil"] == "Especial" || $_SESSION["perfil"] == "Vendedor") {
